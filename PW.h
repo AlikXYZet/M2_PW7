41455f9b05014c5d1e0fd32bfc1261bc27c426cf
Александр Осипов <alikxyzet@gmail.com>
2024-06-19 13:04:34 +0300

    7.0.1 Code Correction: Universal "print" for stl containers

--- PW.h
+++ PW.h
@@ -6,10 +6,28 @@
 #include <string>
 #include <iostream>
 
+
+/*---   Шаблон на проверку типа: Является ля контейнером   ---*/
+template<typename, typename = void>
+constexpr bool is_container = false;
+
+template<typename T>
+constexpr bool is_container<
+	T,
+	std::void_t<
+	decltype(std::declval<T>().begin()),
+	decltype(std::declval<T>().end()),
+	typename T::value_type
+	>
+> = true;
+//----------------------------------------------------------------------------------------------------
+
+
 /*---   Предварительная инициализация   ---*/
 // Функция вывода всех элементов контейнера в консоль
-template<class T>
-void print_list(T& L)
+template<class T>					// Шаблон
+std::enable_if_t <is_container<T>>	// Органичение использования по типу
+print_list(T& L)
 {
 	std::cout << "{ ";
 	std::for_each(L.begin(), L.end(), [](const T::value_type& elem)
